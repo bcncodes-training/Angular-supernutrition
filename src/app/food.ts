aa6bb3dd67a3e81874fc0728637999e17ce2558c
JCJV2019 <cjordanvergara@outlook.es>
2019-07-01 23:40:51 +0200

    Hasta 4ª Iteración

---
+++ src/app/food.ts
@@ -0,0 +1,8 @@
+export class FOOD {
+  name: string;
+  calories: number;
+  image: string;
+  quantity: number;
+}
+
+export default FOOD;
---
+++ src/app/pipes/Filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { BusquedaPipe } from './Filter.pipe';
+
+describe('BusquedaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new BusquedaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
